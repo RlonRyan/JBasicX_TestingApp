4c40249e6b2e347c6c82fb6cde2d9dcd929642f2
RlonRyan <rlonryan@aol.com>
2014-08-27 16:56:10 -0600

    Reworked Networking
    Makes a bit more sense now.

--- src/TestingApp/JBasicX_NetTest.java
+++ src/TestingApp/JBasicX_NetTest.java
@@ -8,10 +8,21 @@ import JNetX.JNetworkListenerX;
 import JNetX.JPacketX.JPackectX;
 import JNetX.JPacketX.JPacketFieldX;
 import JNetX.JPacketX.JPacketTypeX;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /*
  * @author RlonRyan
@@ -40,21 +51,29 @@ public class JBasicX_NetTest extends Thread implements JNetworkListenerX {
      */
     @Override
     public void run() {
-        JClientX clientX = new JClientX(InetAddress.getLoopbackAddress(), 7654);
+        JClientX clientX;
+
+        try {
+            clientX = new JClientX(InetAddress.getByName(""), 7654);
+        } catch (UnknownHostException e) {
+            return;
+        }
+
         clientX.addListener(this);
-        clientX.run();
-        
+        clientX.start();
+
         String cmd = "";
         Scanner reader = new Scanner(System.in);
-        
-        while(!cmd.equalsIgnoreCase("quit")) {
+
+        while (!cmd.equalsIgnoreCase("quit")) {
             cmd = reader.nextLine();
             JPackectX packet;
-            
-            switch(cmd.length()) {
+
+            switch (cmd.length()) {
                 case 1:
                     packet = new JPackectX(JPacketTypeX.UPDATE);
                     packet.set(JPacketFieldX.KEY, cmd.getBytes());
+                    System.out.println(Arrays.toString(cmd.getBytes()));
                     clientX.sendPacket(packet);
                     break;
                 default:
@@ -76,6 +95,47 @@ public class JBasicX_NetTest extends Thread implements JNetworkListenerX {
     }
 
     public static void main(String[] args) {
-        new JBasicX_NetTest(7654).start();
+        DatagramSocket socket;
+        String desired, external;
+        InetAddress address;
+
+        try {
+            URL checkip = new URL("http://checkip.amazonaws.com/");
+            BufferedReader in = new BufferedReader(new InputStreamReader(checkip.openStream()));
+            desired = InetAddress.getByName("rlonryan.selfip.com").getHostAddress();
+            external = in.readLine();
+            Logger.getLogger("Network").log(Level.INFO, "Desired: {0}\nThis Computer: {1}", new Object[]{desired, external});
+            address = (desired.equalsIgnoreCase(external) ? InetAddress.getLoopbackAddress() : InetAddress.getByName(desired));
+            Logger.getLogger("Network").log(Level.INFO, "Using: {0}", address.getHostAddress());
+
+        } catch (IOException e) {
+            Logger.getLogger("Network").log(Level.SEVERE, "Unable to resolve host address!");
+            return;
+        }
+
+        try {
+            socket = new DatagramSocket(7655, address);
+            DatagramPacket packet;
+            JPackectX p = new JPackectX(JPacketTypeX.LOGON);
+            
+            socket.send(p.convert(address, 7654, (byte) 0, null, (byte) 0));
+
+            // get a few quotes
+            for (int i = 0; i < 5; i++) {
+
+                byte[] buf = new byte[256];
+                packet = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+                p = new JPackectX(packet);
+            }
+
+            p = new JPackectX(JPacketTypeX.LOGOFF);
+            socket.send(p.convert(address, 7654, (byte) 0, null, (byte) 0));
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        //new JBasicX_NetTest(7654).start();
     }
 }
--- src/TestingApp/JBasicX_TestingApp.java
+++ src/TestingApp/JBasicX_TestingApp.java
@@ -2,10 +2,16 @@ package TestingApp;
 
 import JGameEngineX.JGameEngineX;
 import JNetX.JHostX;
+import JNetX.JPacketX.JPackectX;
+import JNetX.JPacketX.JPacketFieldX;
+import JNetX.JPacketX.JPacketTypeX;
 import Modes.Main_Game;
 import Modes.Main_Menu;
 import Modes.Pause_Menu;
+import java.io.IOException;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * @author RlonRyan
@@ -15,7 +21,7 @@ import java.util.Random;
  * @info Powered by JBasicX
  *
  */
-public class JBasicX_TestingApp{
+public class JBasicX_TestingApp {
 
     public static JGameEngineX instance;
     public static final String[] options = {"Lambda Style!", "Javaaa!", "Spaaaaaace!", "Generic!", "Automated!", "Title goes here."};
@@ -27,24 +33,27 @@ public class JBasicX_TestingApp{
         }
 
         String mode = args.length >= 1 ? args[0] : "windowed";
-        
+
         int fps = args.length >= 3 ? Integer.parseInt(args[2]) : 100;
         int width = args.length >= 4 ? Integer.parseInt(args[3]) : 640;
         int height = args.length >= 5 ? Integer.parseInt(args[4]) : 480;
         instance = new JGameEngineX("JBasicX Testing Application: " + options[new Random().nextInt(options.length)], mode, fps, width, height);
-        
+
         instance.registerGameMode(new Main_Menu(instance));
         instance.registerGameMode(new Main_Game(instance));
         instance.registerGameMode(new Pause_Menu(instance));
-        
+
         instance.init();
-        
+
         instance.start("main_menu");
-        
-        JHostX host = new JHostX(7654);
-        host.addListener((Main_Game)instance.getGameMode("main_game"));
-        host.run();
-        
+
+        JHostX host;
+        try {
+            host = new JHostX(7654);
+        } catch (IOException e) {
+            return;
+        }
+        host.start();
     }
 
 }
